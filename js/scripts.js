fd4a13857010d4786fb8edb1fcea61a1b7b25ed6
Molly Waggett & Summer Brochtrup <student@epicodus.com>
2015-09-14 14:27:52 -0700

    Write functions to see if input is one word, convert to uppercase and remove punctuation, and output blanks based in the word length; specs are met. Create web interface to display blanks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
-// var functionName = function(params) {
-//
-// };
+var isOneWord = function(word) {
+  if (word.includes(" ")) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-// $(document).ready(function() {
-//
-// });
+var wordToUpperCase = function(word) {
+  return word.replace(/[^\w]|_/g, "").toUpperCase();
+}
+
+var wordToBlanks = function(word) {
+  var blanks = "";
+  for (var i = 0; i < word.length; i++) {
+    blanks = blanks.concat("_ ");
+  }
+  return blanks;
+}
+
+$(document).ready(function() {
+  $("form#mystery-word").submit(function(event){
+    var mysteryWord = $("input#word").val();
+
+    $(".blanks").text(wordToBlanks(mysteryWord));
+
+    $("#start-word").hide();
+    $("#game").show();
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,25 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('isOneWord', function() {
+  it("outputs an error message when multiple words are entered", function() {
+    expect(isOneWord("Sequoia Tree")).to.equal(false);
+  });
+
+  it("outputs true when a single word is entered", function() {
+    expect(isOneWord("Sequoia")).to.equal(true);
+  });
+});
+
+describe('wordToUpperCase', function() {
+  it("turns word into char array of captial letters", function() {
+    expect(wordToUpperCase("sequoia")).to.equal("SEQUOIA");
+  });
+
+  it("removes punctuation", function() {
+    expect(wordToUpperCase("sequoia!!")).to.equal("SEQUOIA");
+  });
+});
+
+describe('wordToBlanks', function() {
+  it("outputs number of blanks corresponding to word length", function() {
+    expect(wordToBlanks("sequoia")).to.equal("_ _ _ _ _ _ _ ");
+  })
+})
